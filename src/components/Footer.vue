9823397b2b5c38d2e3f56d7768a29188cce894a9
Volkov Alexander <volkov.525@gmail.com>
2020-03-03 14:31:31 +0300

    change font size in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
 v-footer(app inset).justify-space-between
-	.copy Карточек: 133 /
-		span.count Посчитать?
-	.copy.gr Здесь некая полезная информация
-	.copy.gr Веб-клиент v.14.2
+	.copy .dv admin panel
+	.copy memory: 80%, processor: 3%
+	.copy v.0.14.2
 
 </template>
 
@@ -20,6 +19,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '@/assets/css/colors.scss';
+.copy {
+	font-size: .9rem;
+}
 
 </style>
