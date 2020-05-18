18a49375a83f3dd682cb35daefcca6cdb596f711
Volkov Alexander <volkov.525@gmail.com>
2020-05-18 10:00:29 +0300

    move widget to component

---
+++ src/components/Widget.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+v-card.cardd
+	v-icon(small v-if="item.smart" color="#fbd067").smart mdi-lightbulb
+	.hd(v-if="item.text.length") {{ item.text }}
+	.hd(v-else) Widget {{ item.i }}
+	.badge(v-if="item.badge") {{ item.badge }}
+	v-btn(text small @click="goTo(item.id)") go
+	template(v-if="item.smart")
+		.smal(v-show="item.h >= 3 && item.h < 5")
+			i.icon-lookma
+			span look ma!
+		.middl(v-show="item.h >= 5 && item.h < 6")
+			i.icon-coolma
+			span.ml-8 cool
+		.larg(v-show="item.h > 5")
+			i.icon-foo
+			span.display-1(v-show="item.h >=6 && item.w > 2") fun
+			ul
+				li(v-for="n in 30")
+					vac( :start-time="new Date().getTime()" :end-time="new Date().getTime() + 400000000" )
+						template( v-slot:process="{ timeObj }" )
+							span {{ `${timeObj.s}` * n + 138}}
+							span.ml-3 server uptime
+	v-btn(icon small v-show="close" @click="remove(item.id)").close
+		v-icon(x-small) mdi-close
+	.test
+</template>
+
+<script>
+
+export default {
+	props: ['item', 'close'],
+	data () {
+		return {
+
+		}
+	},
+	methods: {
+		remove (e) {
+			this.$emit('remove', e)
+		}
+	}
+}
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/colors.scss';
+
+.cardd {
+	width: 100%;
+	height: 100%;
+	border-radius: .4rem;
+	padding: 1rem;
+	position: relative;
+	overflow: hidden;
+}
+.badge {
+	position: absolute;
+	right: 1rem;
+	top: 1rem;
+	background: $link;
+	padding: .2rem .5rem;
+	color: #fff;
+	border-radius: 3rem;
+}
+
+.smart {
+	position: absolute;
+	top: 2px;
+	left: 2px;
+}
+.smal i {
+	font-size: 4rem;
+}
+.middl i {
+	font-size: 6rem;
+}
+.larg i {
+	font-size: 10rem;
+}
+.close {
+	position: absolute;
+	top: -5px;
+	right: -5px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,30 +13,7 @@ div
 					v-icon mdi-pencil
 				grid-layout(:layout.sync="selected" :col-num="12" :row-height="30" :is-draggable="drag" :is-resizable="resize" :is-mirrored="false" :vertical-compact="true" :margin="[10, 10]" :use-css-transforms="true" )
 					grid-item( v-for="item in selected" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i" :key="item.i" @resized="resizedEvent" ).item
-						v-card.cardd
-							v-icon(small v-if="item.smart" color="#fbd067").smart mdi-lightbulb
-							.hd(v-if="item.text.length") {{ item.text }}
-							.hd(v-else) Widget {{ item.i }}
-							.badge(v-if="item.badge") {{ item.badge }}
-							template(v-if="item.smart")
-								.smal(v-show="item.h >= 3 && item.h < 5")
-									i.icon-lookma
-									span look ma!
-								.middl(v-show="item.h >= 5 && item.h < 6")
-									i.icon-coolma
-									span.ml-8 cool
-								.larg(v-show="item.h > 5")
-									i.icon-foo
-									span.display-1(v-show="item.h >=6 && item.w > 2") fun
-									ul
-										li(v-for="n in 30")
-											vac( :start-time="new Date().getTime()" :end-time="new Date().getTime() + 400000000" )
-												template( v-slot:process="{ timeObj }" )
-													span {{ `${timeObj.s}` * n + 138}}
-													span.ml-3 server uptime
-							v-btn(icon small v-show="closeWidget" @click="removeWidget(item.id)").close
-								v-icon(x-small) mdi-close
-							.test
+						Widget(:item="item" :close="closeWidget" @remove="removeWidget")
 		v-tab-item(v-for="(item,index) in panelItems" :key="index")
 			.empt
 				img(src="@/assets/img/nothing.svg")
@@ -51,6 +28,7 @@ div
 <script>
 import VueGridLayout from 'vue-grid-layout'
 import AddPanel from '@/components/AddPanel'
+import Widget from '@/components/Widget'
 
 export default {
 	data () {
@@ -77,7 +55,8 @@ export default {
 	components: {
 		GridLayout: VueGridLayout.GridLayout,
 		GridItem: VueGridLayout.GridItem,
-		AddPanel
+		AddPanel,
+		Widget
 	},
 	mounted () {
 		window.addEventListener('keydown', this.setOn)
@@ -149,23 +128,6 @@ export default {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	.cardd {
-		width: 100%;
-		height: 100%;
-		border-radius: .4rem;
-		padding: 1rem;
-		position: relative;
-		overflow: hidden;
-	}
-}
-.badge {
-	position: absolute;
-	right: 1rem;
-	top: 1rem;
-	background: $link;
-	padding: .2rem .5rem;
-	color: #fff;
-	border-radius: 3rem;
 }
 .v-tabs-items {
 	padding: 1rem;
@@ -176,25 +138,6 @@ export default {
 .empt {
 	margin-top: 2rem;
 	text-align: center;
-}
-.smart {
-	position: absolute;
-	top: 2px;
-	left: 2px;
-}
-.smal i {
-	font-size: 4rem;
-}
-.middl i {
-	font-size: 6rem;
-}
-.larg i {
-	font-size: 10rem;
-}
-.close {
-	position: absolute;
-	top: -5px;
-	right: -5px;
 }
 
 </style>
