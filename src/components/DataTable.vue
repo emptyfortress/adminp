4a3523db0e5254ad99c64f13bf26d9b6428f6ab3
Volkov Alexander <volkov.525@gmail.com>
2020-05-19 16:09:30 +0300

    add dark theme to table

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,16 +1,46 @@
 <template lang="pug">
 .table
-	v-data-table(:headers="headers" :items="items" disable-pagination).tabs
+	//- Toolbar( :current="current" :group="group" @groupped="setGroup")
+	table.tabs
+		thead
+			tr
+				th.sm
+					v-checkbox.check
+				th(v-for="(header, index) in headers"
+					@mouseover="showByIndex = index"
+					@mouseout="showByIndex = null"
+					) 
+					span {{ header.text }}
+					.over(v-show="showByIndex === index")
+						v-icon(@click="sortByIndex = index") mdi-arrow-down
+						v-icon(v-show="smallFilter === index" @click="smallFilter = null" color="#8B0000") mdi-filter-remove-outline
+						v-icon(@click="filterByIndex = index") mdi-filter-outline
+						v-icon(@click="filterByIndex = null") mdi-pin-outline
+						v-icon(@click="filterByIndex = null") mdi-eye-off
+		tbody
+			tr(v-for="item in items").ro
+				td
+					v-checkbox.check
+				//- td {{ item.number }}
+				td {{ item.date }}
+				td {{ item.level }}
+				td {{ item.module }}
+				td error: Your local changes to the following files would be overwritten by checkout
+				td {{ item.settle }}
 </template>
 
 <script>
+// import Toolbar from '@/components/Toolbar'
 
 export default {
 	props: ['headers', 'items'],
 	data() {
 		return {
-
+			showByIndex: null,
 		}
+	},
+	components: {
+		// Toolbar,
 	},
 }
 
@@ -19,7 +49,84 @@ export default {
 <style scoped lang="scss">
 @import '@/assets/css/colors.scss';
 
-.tabs {
-	border: 1px solid #ccc;
+.theme--dark .tabs {
+	border: 1px solid #666;
+	th {
+		background: #444;
+		color: #aaa;
+		border-right: 1px solid #666;
+	}
+	.ro:hover {
+		background: #3a3a3a;
+	}
 }
+
+.theme--light .tabs {
+	border: 1px solid #ccc;
+	th {
+		background: #ddd;
+		color: #666;
+		border-right: 1px solid #fff;
+	}
+	.ro {
+		background: #fff;
+		&:hover {
+			background: #eee;
+		}
+	}
+	td {
+		border-bottom: 1px solid #eee;
+	}
+}
+
+.tabs {
+	width: 100%;
+	margin-bottom: 6rem;
+	transition: all 0.3s ease;
+	border-collapse: collapse;
+	th {
+		height: 2.5rem;
+		position: sticky;
+		top: 0;
+		text-transform: capitalize;
+		font-size: 0.75rem;
+		text-align: left;
+		padding: 0 1rem;
+		z-index: 3;
+	}
+	.ro {
+		height: 3rem;
+	}
+	td {
+		padding: 0 1rem;
+	}
+}
+.check {
+	height: 24px;
+	margin-top: 0;
+	padding-top: 0;
+}
+.tabs th.sm {
+	text-align: center;
+	width: 40px;
+}
+.over {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	text-align: right;
+	margin-right: 0.5rem;
+	background: rgba(255, 255, 255, 0.6);
+	line-height: 41px;
+	i {
+		margin-right: 4px;
+		color: #999;
+		&:hover {
+			color: black;
+		}
+	}
+}
+
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,111 @@
+<template lang="pug">
+.all
+	hr.line
+	.tool
+		.toolcontent
+			template(v-for="(item, index) in buttons")
+				v-tooltip( bottom )
+					template(v-slot:activator="{ on }")
+						v-btn(icon v-on="on" @click="click(item.click)")
+							i(:class="item.icon")
+					span {{item.text}}
+		.page Страницы:
+			span(v-for="(item, index) in pages" :class="current === index ? 'active' : ''" @click="topage(index)").pag {{ index + 1 }}
+</template>
+
+<script>
+
+export default {
+	props: ['current'],
+	data () {
+		return {
+			num: 100,
+			buttons: [
+				{ text: 'Выбрать', icon: 'icon-select', click: '' },
+				{ text: 'Прочитать все', icon: 'icon-read', click: '' },
+				{ text: 'Группировка', icon: 'icon-multi', click: 'groupped' },
+				{ text: 'Поиск', icon: 'icon-search', click: '' },
+				{ text: 'Обновить', icon: 'icon-refresh', click: '' },
+				{ text: 'Reset', icon: 'icon-empty', click: '' },
+				{ text: 'Экспорт', icon: 'icon-xls', click: '' },
+				{ text: 'Настройки', icon: 'icon-setup', click: '' },
+			],
+		}
+	},
+	computed: {
+		pages () {
+			return Math.floor((this.num * 48) / this.windHeight)
+		},
+		windHeight () {
+			return document.documentElement.clientHeight - 114
+		},
+	},
+	methods: {
+		topage (e) {
+			this.$vuetify.goTo(e * this.windHeight)
+		},
+		click (e) {
+			this.$emit(e)
+		},
+	},
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/colors.scss';
+
+.page {
+	font-size: 1rem;
+	font-weight: 400;
+	background: #e4e4e0;
+	padding: 10px 25px;
+	height: 44px;
+	text-align: right;
+	padding-right: 0;
+}
+.pag {
+	font-size: 0.8rem;
+	border: 1px solid #ccc;
+	margin-right: 3px;
+	padding: 4px 5px;
+	cursor: pointer;
+	border-radius: 2px;
+	&:first-child {
+		margin-left: 1rem;
+	}
+	&.active {
+		background: #fff;
+		font-size: 1rem;
+		padding: 5px 7px;
+		border-color: #aaa;
+	}
+}
+.all {
+	background: #e4e4e0;
+	/* border-top: 1px solid #ccc; */
+	position: sticky;
+	top: 0;
+}
+.tool {
+	display: flex;
+	justify-content: space-between;
+	.toolcontent {
+		height: 44px;
+		line-height: 44px;
+		.v-btn {
+			margin-right: .7rem;
+
+		}
+		i {
+			font-size: 1.2rem;
+			margin-left: -3px;
+		}
+	}
+}
+.line {
+	border: 0;
+	height: 0;
+	border-top: 1px solid rgba(0, 0, 0, 0.23);
+	border-bottom: 1px solid rgba(255, 255, 255, 0.6);
+}
+</style>
