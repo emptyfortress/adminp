aa8c1254ee67d98a660f7a719ee02b148d7a8bf4
Volkov Alexander <volkov.525@gmail.com>
2020-05-26 11:11:28 +0300

    checkout breadcrumbs from other branch

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+v-breadcrumbs(:items="breadcrumbs")
+</template>
+
+<script>
+
+export default {
+	data () {
+		return {
+		}
+	},
+	computed: {
+		breadcrumbs () {
+			return this.$route.meta.breadcrumbs
+		},
+	},
+}
+
+</script>
