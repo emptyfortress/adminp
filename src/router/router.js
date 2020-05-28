5efd28748014f279d0aec4909d88be387b654ebc
Volkov Alexander <volkov.525@gmail.com>
2020-05-28 11:15:41 +0300

    add breadcrumbs to main page

--- src/router/router.js
+++ src/router/router.js
@@ -13,6 +13,11 @@ const routes = [
 		path: '/',
 		name: 'home',
 		component: Home,
+		meta: {
+			breadcrumbs: [
+				{text: 'Главная', href: '/', disabled: true },
+			],
+		},
 	},
 	{
 		path: '/problems',
