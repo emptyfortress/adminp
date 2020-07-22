e9054d2ef9d37dc00e0cb2dcce8acedca58577be
Volkov Alexander <volkov.525@gmail.com>
2020-07-22 13:10:09 +0300

    add table to log page

---
+++ src/components/LogTable.vue
@@ -0,0 +1,220 @@
+<template lang="pug">
+.table
+	//- Toolbar( :current="current" :group="group" @groupped="setGroup")
+	v-row(justify="space-between" align="center")
+		v-col(cols="12" sm="9")
+			v-row
+				v-chip-group(active-class="tt").mr-8
+					v-chip.mr-2 Сегодня
+					v-chip.mr-2 Вчера
+					v-chip.mr-2 Эта неделя
+				v-chip-group(active-class="tt").mr-5
+					v-chip.mr-2 Параметр
+					v-chip.mr-2 Параметр
+					v-chip.mr-2 Параметр
+					v-chip.mr-2 Параметр
+
+		v-col(cols="12" sm="3")
+			v-text-field(v-model="filter" label="Фильтр" prepend-inner-icon="mdi-filter-outline" clearable single-line)
+	table.tabs
+		thead
+			tr
+				th(v-for="(header, index) in headers"
+					@mouseover="showByIndex = index"
+					@mouseout="showByIndex = null"
+					) 
+					span {{ header.text }}
+					.over(v-show="showByIndex === index")
+						v-icon(@click="sortByIndex = index") mdi-arrow-down
+						v-icon(@click="setFilter(index)") mdi-filter-outline
+					v-slide-y-transition
+						v-card.quick.elevation-3(v-show="filterByIndex === index")
+							v-text-field(autofocus clearable :key="index" v-model="colfilter[index]")
+		tbody
+			tr(v-for="item in filteredItems").ro
+				td {{ item.date }}
+				td {{ item.level }}
+				td {{ item.module }}
+				td Action: Your local changes to the following files would be overwritten by checkout
+				td
+					span {{ item.settle }}
+					span.action
+						i.icon-star-empty
+						i.icon-check
+						i.icon-trash-line
+			tr(v-show="filteredItems.length === 0")
+				td(colspan="6")
+					v-img(src="@/assets/img/nothing.svg" width="130").mx-auto.my-3
+					p.text-center.blue-grey--text Ничего не найдено
+
+</template>
+
+<script>
+// import Toolbar from '@/components/Toolbar'
+
+export default {
+	props: ['headers', 'items'],
+	data() {
+		return {
+			showByIndex: null,
+			smallFilter: null,
+			filterByIndex: null,
+			all: this.items,
+			colfilter: [],
+			filter: '',
+		}
+	},
+	mounted () {
+		this.colfilter.length = this.headers.length
+	},
+	computed: {
+		filteredItems () {
+			let result = this.items
+			if (!this.filter) {
+				return result
+			} else {
+				const filterValue = this.filter.toLowerCase()
+				const filt = function (item) {
+					return item.level.toLowerCase().includes(filterValue) ||
+						item.module.toLowerCase().includes(filterValue) ||
+						item.date.toLowerCase().includes(filterValue) ||
+						item.descr.toLowerCase().includes(filterValue)
+				}
+				return result.filter(filt)
+			}
+		},
+	},
+	methods: {
+		setFilter (e) {
+			if (!this.filterByIndex) {
+				this.filterByIndex = e
+			} else this.filterByIndex = null
+		},
+	},
+}
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/colors.scss';
+
+.theme--dark .tabs {
+	border: 1px solid #666;
+	th {
+		background: #444;
+		color: #aaa;
+		border-right: 1px solid #666;
+	}
+	.ro:hover {
+		background: #3a3a3a;
+	}
+	td {
+		border-bottom: 1px solid #666;
+		color: #ccc;
+	}
+}
+
+.theme--light .tabs {
+	border: 1px solid #ccc;
+	th {
+		background: #ddd;
+		color: #666;
+		border-right: 1px solid #fff;
+	}
+	.ro {
+		background: #fff;
+		&:hover {
+			background: #eee;
+		}
+	}
+	td {
+		border-bottom: 1px solid #eee;
+	}
+}
+
+.tabs {
+	width: 100%;
+	margin-bottom: 6rem;
+	transition: all 0.3s ease;
+	border-collapse: collapse;
+	th {
+		height: 2.5rem;
+		position: sticky;
+		top: 0;
+		text-transform: capitalize;
+		font-size: 0.75rem;
+		text-align: left;
+		padding: 0 1rem;
+		z-index: 3;
+	}
+	.ro {
+		height: 3rem;
+		position: relative;
+	}
+	td {
+		padding: 0 1rem;
+		position: relative;
+	}
+}
+.check {
+	height: 24px;
+	margin-top: 0;
+	padding-top: 0;
+}
+.tabs th.sm {
+	text-align: center;
+	width: 40px;
+}
+.over {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	text-align: right;
+	margin-right: 0.5rem;
+	background: #eee;
+	line-height: 41px;
+	i {
+		margin-right: 4px;
+		color: #999;
+		&:hover {
+			color: black;
+		}
+	}
+}
+span.action {
+	display: none;
+	width: 150px;
+	height: 100%;
+	position: absolute;
+	right: 0px;
+	top: 0;
+	background: #eee;
+	text-align: right;
+	padding-right: 1rem;
+	line-height: 45px;
+	font-size: 1.2rem;
+	cursor: pointer;
+	i {
+		margin-left: .5rem;
+	}
+}
+tr:hover span.action {
+	display: block;
+}
+.quick {
+	position: absolute;
+	top: 40px;
+	left: 0;
+	width: 100%;
+	min-width: 200px;
+	max-width: 400px;
+	padding: 0 1rem;
+}
+.v-chip.tt {
+	background: $blue-grey;
+	color: #fff;
+}
+
+</style>
--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -1,58 +1,28 @@
 <template lang="pug">
 .pa-5
-	.zag Logs
-	br
-	br
-	.mygrid
-		.box.box0 0
-		.box.box1 1
-		.box.box2 2
-		.box.box3 3
-		.box.box4 4
+	.zag Последние действия
+	LogTable(:headers="headers" :items="actions")
 </template>
 
+<script>
+import LogTable from '@/components/LogTable'
+import {headers, actions} from '@/actions.js'
+
+export default {
+	data () {
+		return {
+			headers,
+			actions,
+		}
+	},
+	components: {
+		LogTable,
+	},
+}
+
+</script>
+
 <style scoped lang="scss">
-
-.mygrid {
-	display: grid;
-	grid-template-columns: 1fr 3fr 1fr;
-	grid-template-rows: 1fr 2fr 100px;
-	grid-gap: 1rem;
-	grid-template-areas: "header header header"
-												 "test side body"
-												 "test . body"
-												 "footer footer footer";
-}
-
-.box {
-	height: 100%;
-	background: #ccc;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-size: 2rem;
-}
-.box0 {
-	grid-area: header;
-}
-.box1 {
-	grid-area: side;
-}
-.box2 {
-	grid-area: test side;
-	width: 800px;
-	
-	z-index: 2;
-	/* width: 100px; */
-	background: red;
-	opacity: .8;
-}
-.box3 {
-	grid-area: footer;
-}
-.box4 {
-	grid-area: body;
-}
 
 
 </style>
