ecafd8dec6299d31637ae227bf6472ea38563cff
Volkov Alexander <volkov.525@gmail.com>
2020-08-10 13:20:06 +0300

    add component
      modified:   src/views/Dash.vue

---
+++ src/components/WidgGraph.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.widg
+	v-select(:items="database" v-model="d1" prepend-icon="mdi-database" dense placeholder="Database" ).dtb
+	img(src="@/assets/img/disconnected.svg" v-show="d1.length === 0")
+</template>
+
+<script>
+
+export default {
+	props: ['database', 'model'],
+	data () {
+		return {
+			d1: [],
+		}
+	},
+}
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/colors.scss';
+
+
+</style>
